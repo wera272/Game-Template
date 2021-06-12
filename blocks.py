03acd273c7f173cc1a0e5a59043935ed1a0a4a66
wera272 <82270518+wera272@users.noreply.github.com>
2021-06-12 15:04:14 +0200

    movements

--- blocks.py
+++ blocks.py
@@ -131,7 +131,39 @@ class Blocks(object):  #
         self.shape = shape
         self.color = shape_colors[shapes.index(shape)]
         self.rotation = 0
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
 
 def get_shape():
     return Blocks(5, 0, random.choice(shapes))
+
+def draw_next_shape(shape, surface):
+    font = pygame.font.SysFont('Berlin Sans FB', 30)
+    label = font.render('Next Shape', 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy + i*block_size, block_size, block_size), 0)
+
+    surface.blit(label, (sx + 10, sy - 30))
+
+
 pygame.display.update()
